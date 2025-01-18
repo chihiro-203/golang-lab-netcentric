a9ee56e3f0b28d4fd0e5649d054fb86c3ce6d2e5
ume <chi.nguyenthiyen.203@gmail.com>
2025-01-19 01:05:16 +0700

    Exercise 4 - Lab 1

--- Lab1/Ex4/main.go
+++ Lab1/Ex4/main.go
@@ -1,18 +1,76 @@
 package main
 
-type rect struct {
-	width, height int
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func printGrid(grid [][]rune) {
+	for i := range grid {
+		for j := range grid[i] {
+			fmt.Printf("%c ", grid[i][j])
+		}
+		fmt.Println()
+	}
 }
 
-func (r rect) changeByVal() {
-	r.width = 0
-	r.height = 0
+func generateGrid(rows, cols, mines int) [][]rune {
+	rand.Seed(time.Now().UnixNano())
+
+	grid := make([][]rune, rows)
+	for i := range grid {
+		grid[i] = make([]rune, cols)
+		for j := range grid[i] {
+			grid[i][j] = '.'
+		}
+	}
+
+	placed := 0
+	for placed < mines {
+		r := rand.Intn(rows)
+		c := rand.Intn(cols)
+		if grid[r][c] != '*' {
+			grid[r][c] = '*'
+			placed++
+		}
+	}
+
+	return grid
 }
 
-func (r rect) changeByRef() {
-	r.width = 0
-	r.height = 0
+func aroundMines(grid [][]rune) [][]rune {
+	directions := [][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	rows := len(grid)
+	cols := len(grid[0])
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			count := 0
+			if grid[i][j] == '.' {
+				for _, dir := range directions {
+					r, c := i+dir[0], j+dir[1]
+					if r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '*' {
+						count++
+						if count > 0 {
+							grid[i][j] = rune('0' + count)
+						}
+					}
+				}
+			}
+		}
+	}
+	return grid
 }
 
 func main() {
+	grid := generateGrid(20, 25, 99)
+	printGrid(grid)
+	fmt.Println()
+	grid = aroundMines(grid)
+	printGrid(grid)
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Labs for Net-centric Programming
 This repository contains net-centric labs implemented in Go with assistance from AI tools. While significant effort has been made to ensure functionality, there may still be unresolved errors or areas for improvement. If you plan to use or build upon this code, please review it thoroughly and feel free to contribute any fixes or suggestions. Thank you for your understanding and support!
+
+## Lab 1 - Introduction to Golang
