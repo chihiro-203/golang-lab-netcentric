84ccac7a14289327d62c1573eb3e7721faefcfdf
ume <chi.nguyenthiyen.203@gmail.com>
2025-01-19 22:17:05 +0700

    Exercise 5 - Lab 1

--- Lab1/Ex5/main.go
+++ Lab1/Ex5/main.go
@@ -1,3 +1,42 @@
+// Matching Brackets
+
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Print("A string with brackets: ")
+	reader := bufio.NewReader(os.Stdin)
+	str, _ := reader.ReadString('\n')
+
+	pairs := map[rune]rune{
+		']': '[',
+		'}': '{',
+		')': '(',
+	}
+
+	stack := []rune{}
+
+	for _, char := range str {
+		if char == '[' || char == '{' || char == '(' {
+			stack = append(stack, char)
+		} else if char == ']' || char == '}' || char == ')' {
+			if len(stack) == 0 || pairs[char] != stack[len(stack)-1] {
+				fmt.Println("Incorrect!")
+				return
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	if len(stack) == 0 {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Incorrect!")
+	}
+
+}
