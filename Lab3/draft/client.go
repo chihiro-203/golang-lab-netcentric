332ccb68c6a3f370be05b14934609bc0545aba38
ume <chi.nguyenthiyen.203@gmail.com>
2025-02-03 00:06:54 +0700

    Lab 3 - Redo

---
+++ Lab3/draft/client.go
@@ -0,0 +1,328 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var key string
+
+func hashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error connecting to server", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to server.")
+	fmt.Println("You need to login or sign up to send message.")
+	fmt.Println("- Enter '/login' to log in")
+	fmt.Println("- Enter '/register' to sign up")
+
+	// authenticated := false
+
+	for {
+		fmt.Print("> ")
+		reader := bufio.NewReader(os.Stdin)
+		message, _ := reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		if message == "/login" {
+			login(conn)
+			break
+		} else if message == "/register" {
+			register(conn)
+			break
+		} else {
+			fmt.Println("You need to login or sign up to send a message.")
+			fmt.Println("- Enter '/login' to log in")
+			fmt.Println("- Enter '/register' to sign up")
+		}
+	}
+}
+
+func login(conn net.Conn) {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Logging in...")
+		fmt.Print("Enter username: ")
+		username, _ := reader.ReadString('\n')
+		username = strings.TrimSpace(username)
+
+		fmt.Print("Enter password: ")
+		password, _ := reader.ReadString('\n')
+		password = strings.TrimSpace(password)
+
+		message := "/login " + username + " " + password
+		_, err := conn.Write([]byte(message + "\n"))
+		if err != nil {
+			fmt.Println("Error sending login data to server:", err)
+			return
+		}
+
+		response, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from server:", err)
+			return
+		}
+
+		if strings.Contains(strings.TrimSpace(response), "failed") {
+			fmt.Printf("Server: %s", response)
+			continue
+		}
+
+		response = strings.TrimSpace(response)
+
+		key = strings.TrimSpace(response)
+
+		fmt.Printf("%s_Login successfully. Your key: %s\n", key, response)
+		fmt.Println(key, "_To continue, please follow instructions below:")
+		fmt.Println("- Enter '/profile' to modify your profile")
+		fmt.Println("- Enter '/game' to play Guessing Game")
+		fmt.Println("- Enter '/file' to play download our file")
+
+		fmt.Print("> ")
+		reader := bufio.NewReader(os.Stdin)
+		message, _ = reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		writeMsg(conn, message)
+
+		if message == "/profile" {
+			modifyProfile(conn)
+		} else if message == "/game" {
+			guessingGame(conn)
+		} else if message == "/file" {
+			downloadFile(conn)
+		}
+
+		break
+	}
+}
+
+func register(conn net.Conn) {
+	fmt.Println("Registering a new account...")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter username: ")
+	username, _ := reader.ReadString('\n')
+	username = strings.TrimSpace(username)
+
+	var password, cPassword string
+	for {
+		fmt.Print("Enter password: ")
+		password, _ = reader.ReadString('\n')
+		password = strings.TrimSpace(password)
+
+		fmt.Print("Confirm password: ")
+		cPassword, _ = reader.ReadString('\n')
+		cPassword = strings.TrimSpace(cPassword)
+
+		if password == cPassword {
+			// hashed, err := hashPassword(password)
+			// if err != nil {
+			// 	fmt.Println("Error hashing password:", err)
+			// }
+			// message := "/register " + username + " " + hashed
+			// _, err = conn.Write([]byte(message + "\n"))
+			// if err != nil {
+			// 	fmt.Println("Error sending registration data to server:", err)
+			// 	return
+			// }
+
+			// response, err := bufio.NewReader(conn).ReadString('\n')
+			// if err != nil {
+			// 	fmt.Println("Error reading from server:", err)
+			// 	return
+			// }
+			// fmt.Printf("Server:  %s", response)
+			break
+		} else {
+			fmt.Println("Passwords do not match. Please try again.")
+		}
+	}
+
+	hashed, err := hashPassword(password)
+	if err != nil {
+		fmt.Println("Error hashing password:", err)
+	}
+	message := "/register " + username + " " + string(hashed[:])
+	_, err = conn.Write([]byte(message + "\n"))
+	if err != nil {
+		fmt.Println("Error sending registration data to server:", err)
+		return
+	}
+
+	response, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from server:", err)
+		return
+	}
+	fmt.Printf("Server:  %s", response)
+}
+
+func writeMsg(conn net.Conn, message string) {
+	_, err := conn.Write([]byte(message + "\n"))
+	if err != nil {
+		fmt.Println("Error sending login data to server:", err)
+		return
+	}
+}
+
+func readMsg(conn net.Conn) string {
+	response, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from server:", err)
+		return ""
+	}
+	return response
+}
+
+func modifyProfile(conn net.Conn) {
+	fmt.Println("Modifying your profile...")
+
+	fmt.Print("> ")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Username ('no' if not change): ")
+	message, _ := reader.ReadString('\n')
+	message = strings.TrimSpace(message)
+	writeMsg(conn, message)
+
+	fmt.Println("Fullname ('no' if not change): ")
+	message, _ = reader.ReadString('\n')
+	message = strings.TrimSpace(message)
+	writeMsg(conn, message)
+
+	fmt.Println("Email ('no' if not change): ")
+	message, _ = reader.ReadString('\n')
+	message = strings.TrimSpace(message)
+	writeMsg(conn, message)
+
+	fmt.Println("Address ('no' if not change): ")
+	message, _ = reader.ReadString('\n')
+	message = strings.TrimSpace(message)
+	writeMsg(conn, message)
+
+	fmt.Println("Do you want to change password? \n'yes' if change\n'no' if not change")
+	message, _ = reader.ReadString('\n')
+	message = strings.TrimSpace(message)
+	writeMsg(conn, message)
+	if message == "yes" {
+		fmt.Println("Old password: ")
+		message, _ = reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+		writeMsg(conn, message)
+
+		fmt.Println("New password: ")
+		message, _ = reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+		writeMsg(conn, message)
+	}
+}
+
+func guessingGame(conn net.Conn) {
+	fmt.Println("Starting game...")
+
+	var num string
+
+gameLoop:
+	for {
+		fmt.Print(key, "_Input your guessed number: ")
+		fmt.Scan(&num)
+
+		writeMsg(conn, num)
+		response := readMsg(conn)
+		fmt.Print(response)
+		os.Stdout.Sync()
+
+		if strings.Contains(strings.ToLower(response), "correct") {
+			answer := ""
+			for answer != "yes" && answer != "no" {
+				fmt.Println(key, "_Do you want to play again? (yes/no)")
+				fmt.Scan(&answer)
+				answer = strings.ToLower(answer)
+				if answer == "yes" {
+					writeMsg(conn, answer)
+					continue
+				} else if answer == "no" {
+					writeMsg(conn, answer)
+					fmt.Println(key, "_Thanks for playing the Guessing game.")
+					break gameLoop
+				} else {
+					fmt.Println(key, "_Please type the correct syntax.")
+				}
+			}
+		} else {
+			continue
+		}
+	}
+
+}
+
+func downloadFile(conn net.Conn) {
+	fmt.Println("Looking for available files...")
+
+	for {
+		files := readMsg(conn)
+		fmt.Println("Our available files: ", files)
+
+		var filename string
+		fmt.Print(key, "_Which file you want to download? ")
+		fmt.Scan(&filename)
+
+		writeMsg(conn, filename)
+
+		filename = "d_" + filename
+		localFile, err := os.Create(filename)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer localFile.Close()
+
+		_, err = io.Copy(localFile, conn)
+		if err != nil {
+			fmt.Println("Error downloading file:", err)
+			return
+		}
+		fmt.Println("File downloaded successfully as:", filename)
+		response := readMsg(conn)
+		fmt.Println(response)
+
+		answer := ""
+		for answer != "yes" && answer != "no" {
+			fmt.Println(key, "_Do you want to download more? (yes/no)")
+			fmt.Scan(&answer)
+			answer = strings.ToLower(answer)
+
+			if answer == "yes" {
+				writeMsg(conn, answer)
+				continue
+			} else if answer == "no" {
+				writeMsg(conn, answer)
+				fmt.Println(key, "_Thanks for downloading our files.")
+				return
+			} else {
+				fmt.Println(key, "_Please type the correct syntax.")
+			}
+		}
+	}
+}
---
+++ Lab3/draft/server.go
@@ -0,0 +1,293 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	authenticated     = make(map[string]int)
+	isClientConnected bool
+	mu                sync.Mutex
+	userFile          = "users.json"
+	name              string
+)
+
+type User struct {
+	Username string   `json:"username"`
+	Password string   `json:"password"`
+	FullName string   `json:"fullname"`
+	Email    []string `json:"email"`
+	Address  []string `json:"address"`
+	Prefix   int      `json:"prefix"`
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error listening to client:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Println("Server is listening on port 8080...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+		}
+
+		// Check if any clients try to connect.
+		mu.Lock()
+		if isClientConnected {
+			fmt.Println("New client tried to connect, but a client is already connected.")
+			conn.Write([]byte("Server is busy. Try again later.\n"))
+			conn.Close()
+			mu.Unlock()
+			continue
+		}
+		isClientConnected = true
+		mu.Unlock()
+
+		fmt.Println("Client connected!")
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	mu.Lock()
+	isClientConnected = true
+	defer func() {
+		isClientConnected = false
+		mu.Unlock()
+	}()
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Client disconnected!")
+			return
+		}
+		message = strings.TrimSpace(message)
+		parts := strings.Split(message, " ")
+		action, username, password := parts[0], parts[1], parts[2]
+
+		if action == "/register" {
+			if userRegister(username, password) {
+				conn.Write([]byte("Registration successful. Please login.\n"))
+			} else {
+				conn.Write([]byte("Registration failed. Username may already exist.\n"))
+			}
+		} else if action == "/login" {
+			prefix := userLogin(username, password)
+			if prefix != 0 {
+				fmt.Printf("Client's username: %s\n", username)
+				name = username
+				authenticated[username] = prefix
+				conn.Write([]byte(fmt.Sprintf("%d\n", prefix)))
+				message = receiveMsg(conn)
+				if message == "/profile" {
+					modifyInfo()
+				} else if message == "/game" {
+					playGame(conn)
+				} else if message == "/file" {
+					getFiles(conn)
+				}
+			} else {
+				conn.Write([]byte("Login failed. Invalid username or password.\n"))
+			}
+		} else {
+			conn.Write([]byte("Unknown command.\n"))
+		}
+	}
+}
+
+func loadUsers() []User {
+	var users []User
+
+	if _, err := os.Stat(userFile); os.IsNotExist(err) {
+		return users
+	}
+
+	data, err := ioutil.ReadFile(userFile)
+	if err != nil {
+		fmt.Println("Error reading user file:", err)
+		return users
+	}
+
+	err = json.Unmarshal(data, &users)
+	if err != nil {
+		fmt.Println("Error decoding user JSON:", err)
+		return users
+	}
+
+	return users
+}
+
+func saveUser(users []User) {
+	data, err := json.MarshalIndent(users, "", " ")
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+
+	err = ioutil.WriteFile(userFile, data, 0644)
+	if err != nil {
+		fmt.Println("Error writing to user file:", err)
+	}
+}
+
+func userRegister(username, password string) bool {
+	users := loadUsers()
+
+	for _, user := range users {
+		if user.Username == username {
+			return false
+		}
+	}
+
+	prefix := genNum()
+
+	newUser := User{
+		Username: username,
+		Password: password,
+		Prefix:   prefix,
+	}
+
+	users = append(users, newUser)
+	saveUser(users)
+
+	return true
+}
+
+func userLogin(username, password string) int {
+	users := loadUsers()
+
+	username = strings.TrimSpace(username)
+	password = strings.TrimSpace(password)
+
+	for _, user := range users {
+		if user.Username == username && bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) == nil {
+			return user.Prefix
+		}
+	}
+
+	return 0
+}
+
+func modifyInfo() {}
+
+func playGame(conn net.Conn) {
+	randNum := genNum()
+	fmt.Printf("The random number is: %d\n", randNum)
+
+	for {
+		reader := bufio.NewReader(conn)
+		message, _ := reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+
+		answer, _ := strconv.Atoi(message)
+
+		if answer > randNum {
+			conn.Write([]byte("Your guessed number is larger than the result. Please try again.\n"))
+		} else if answer < randNum {
+			conn.Write([]byte("Your guessed number is smaller than the result. Please try again.\n"))
+		} else {
+			conn.Write([]byte(fmt.Sprintf("Correct answer. The random number is %d.\n", randNum)))
+
+			response, _ := reader.ReadString('\n')
+			response = strings.TrimSpace(response)
+			os.Stdout.Sync()
+			if response == "yes" {
+				randNum = genNum()
+				fmt.Printf("The random number is: %d\n", randNum)
+				continue
+			} else if response == "no" {
+				fmt.Println(name, "finishes playing game.")
+				break
+			}
+		}
+	}
+}
+
+func getFiles(conn net.Conn) {
+	folderPath := "./files"
+
+	for {
+		files, err := os.ReadDir(folderPath)
+		if err != nil {
+			fmt.Println("Error reading folder:", err)
+		}
+
+		var names []string
+
+		for _, file := range files {
+			if !file.IsDir() {
+				names = append(names, file.Name())
+			}
+		}
+
+		namesString := strings.Join(names, "  ")
+		conn.Write([]byte(namesString + "\n"))
+
+		filename := receiveMsg(conn)
+		fmt.Println(authenticated[name], "_I choose", filename)
+
+		filename = filepath.Join(folderPath, filename)
+
+		file, err := os.Open(filename)
+		if err != nil {
+			conn.Write([]byte("File not found.\n"))
+			fmt.Println("File not found:", filename)
+			return
+		}
+		defer file.Close()
+
+		_, err = io.Copy(conn, file)
+		if err != nil {
+			fmt.Println("Error sending file:", err)
+			return
+		}
+
+		fmt.Println("File sent successfully:", filename)
+
+		conn.Write([]byte("Ask for downloading more.\n"))
+		answer := receiveMsg(conn)
+		if answer != "yes" {
+			fmt.Println("Client chose not to download more files.")
+			break
+		}
+	}
+
+}
+
+func genNum() int {
+	// rand.Seed(time.Now().UnixNano())
+	return rand.Intn(100) + 1
+}
+
+func receiveMsg(conn net.Conn) string {
+	response, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from server:", err)
+		return ""
+	}
+	response = strings.TrimSpace(response)
+	return response
+}
