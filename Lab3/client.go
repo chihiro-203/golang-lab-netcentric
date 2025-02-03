388eac362e545e9ff938b9d1d0580217d2f0bcae
ume <chi.nguyenthiyen.203@gmail.com>
2025-02-03 23:31:44 +0700

    Lab 3 - First connection

--- Lab3/client.go
+++ Lab3/client.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error connecting to server", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
 }
--- Lab3/server.go
+++ Lab3/server.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error listening to client:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Println("Server is listening on port 8080...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+		}
+	}
+}
